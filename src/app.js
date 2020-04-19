df2124b968f4af2b8dc45562e859e88882f21dc9
Charmaine <charmaine@hts-macbook-pro.home>
2020-04-19 15:59:27 +0100

    css added

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 const express = require ('express');
 const exphbs = require ('express-handlebars');
+const path = require ('path');
 const port = 3000;
 
 const app = express();
 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
 
-app.get('/', (req, res) => res.send('Hello World!!!'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine('hbs', exphbs({
+    extname: 'hbs',
+    layoutsDir: path.join(__dirname, "views", "layouts"),
+}));
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.get('/', (req, res) => res.render('home'));
 app.listen(port, () => console.log(`We have lift off, landingPage is listening at port ${port}`));
